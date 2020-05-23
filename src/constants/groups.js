fb8c9276d3b854eea279335164d1549c60a08ae3
Michael Beigelmacher <brooklynbagel@gmail.com>
2020-05-23 13:38:37 -0400

    Revise groups to have displayed name and internal group

--- src/constants/groups.js
+++ src/constants/groups.js
@@ -2,7 +2,7 @@
 // NOTE THAT THE FIRST LETTER SHOULD BE CAPITALIZED
 // TO FIT IN WITH STRING SCHEMA
 export const groups = [
-	'USA',
-	'Mexico',
-	'Burning Man',
+	{ name: 'USA', group: 'leveler' },
+	{ name: 'Mexico', group: 'mx' },
+	{ name: 'Burning Man', group: 'burners' },
 ];
